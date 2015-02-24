70619295f0067a75faf128325db169dd535b2882
cgkaczenski <kzpulaski@gmail.com>
2015-02-24 01:10:52 -0600

    learning express

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var express = require('express');
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', 8080);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('Meadowlark Travel');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('About Meadowlark Travel');
+});
+
+
+app.use(function(req,res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(req,res) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - server error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started..'+ app.get('port'));
 });
